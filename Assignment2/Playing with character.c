9e3ee1d88454995498f1b1a464529fef696d1b9a
Wulakey Tanwar <113882113+wulakey-tanwar@users.noreply.github.com>
2022-09-25 11:46:54 +0530

    Create Playing with character.c

---
+++ Assignment2/Playing with character.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    char a;
+    scanf("%c",&a);
+    char b[10];
+    scanf("%s",b);
+    printf("%c\n%s\n",a,b);
+    char str[100];
+     getchar() ;
+     gets(str);
+    
+    printf("%s",str);
+    
+  
+
+}
+   
+    
+    
+    
+
