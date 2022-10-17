0c73773b42f694a001fe99b789e0352f449df9f2
Wulakey Tanwar <113882113+wulakey-tanwar@users.noreply.github.com>
2022-10-17 20:53:53 +0530

    Create Insert node at tail of linked list.cpp

---
+++ Link List/Insert node at tail of linked list.cpp
@@ -0,0 +1,18 @@
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode* temp= new SinglyLinkedListNode(data);
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+    }
+    else {
+        temp->data=data;
+        SinglyLinkedListNode* p;
+        p=head;
+        while(p->next!=NULL){
+            p=p->next;
+        }
+        p->next=temp;
+        }
+        return head ;
+}
