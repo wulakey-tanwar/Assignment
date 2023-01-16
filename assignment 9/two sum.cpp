6d51f334854bef13e84fea65b7c809886dbc0718
Wulakey Tanwar <113882113+wulakey-tanwar@users.noreply.github.com>
2023-01-16 15:18:23 +0530

    Create two sum.cpp

---
+++ assignment 9/two sum.cpp
@@ -0,0 +1,17 @@
+#include <unordered_map>
+ 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+       
+        for(int i = 0; i < nums.size(); i++){
+            if(m.find(target - nums[i]) == m.end())
+                m[nums[i]] = i;
+            else
+                return {m[target - nums[i]], i};
+        }
+ 
+        return {-1, -1};
+    }
+};
