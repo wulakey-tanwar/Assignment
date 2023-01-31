8da7e4f2fddb20e564bce65d9dea540f8246bc53
Wulakey Tanwar <113882113+wulakey-tanwar@users.noreply.github.com>
2023-01-31 12:24:40 +0530

    Add files via upload

---
+++ Assignment 10/height of binary tree.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        if(!root)
+            return 0;
+        if(!root->left && !root->right)
+           return 0;
+        int h1=height(root->left);
+        int h2=height(root->right);
+        int ans=max(h1,h2)+1;
+        return ans;
+    }
+
+}; //End of Solution
